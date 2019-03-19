08740d718dddb2e3d5fbe8aafd7a7f43f58729fe
Caleb Taylor <aquaductape@gmail.com>
2019-03-18 17:41:34 -0700

    app is now desktop responsive

--- src/components/FaceRecognition/ImagePanel/ImageDemo.js
+++ src/components/FaceRecognition/ImagePanel/ImageDemo.js
@@ -4,12 +4,14 @@ import BoundingBox from './Bounding/BoundingBox';
 const ImageDemo = ({ imageUrl, onMainImageLoad, boundingBox }) => {
 	return (
 		<div className="container">
-			<img onLoad={(e) => onMainImageLoad(e, boundingBox)} className="image-demo" src={imageUrl} alt="" />
-			{boundingBox.map(({ region_info }) => {
-				const boundingBox = region_info.bounding_box;
-				const id = `${boundingBox.top_row}${boundingBox.left_col}${boundingBox.bottom_row}${boundingBox.right_col}`;
-				return <BoundingBox key={id} boundingBox={region_info.bounding_box} />;
-			})}
+			<div className="container--image-demo">
+				<img onLoad={(e) => onMainImageLoad(e, boundingBox)} className="image-demo" src={imageUrl} alt="" />
+				{boundingBox.map(({ region_info }) => {
+					const boundingBox = region_info.bounding_box;
+					const id = `${boundingBox.top_row}${boundingBox.left_col}${boundingBox.bottom_row}${boundingBox.right_col}`;
+					return <BoundingBox key={id} boundingBox={region_info.bounding_box} />;
+				})}
+			</div>
 		</div>
 	);
 };
--- src/components/FaceRecognition/ImagePanel/InfoDemo.js
+++ src/components/FaceRecognition/ImagePanel/InfoDemo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BoundingInfo from './Bounding/BoundingInfo';
+import Stats from '../../Stats/Stats';
 
 const InfoDemo = ({ boundingBox, onCanvas }) => {
 	return (
 		<div className="info-demo">
+			<Stats className="stats--info-demo" display={null} boundingBox={boundingBox} />
 			{boundingBox.map(({ region_info }) => {
 				const boundingBox = region_info.bounding_box;
 				const id = `${boundingBox.top_row}${boundingBox.left_col}${boundingBox.bottom_row}${boundingBox.right_col}`;
