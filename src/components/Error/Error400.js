55247430d2bc2d8b8684204120bbe192c5097636
Caleb Taylor <aquaductape@gmail.com>
2019-03-20 19:02:18 -0700

    fixed Error display

--- src/components/Error/Error400.js
+++ src/components/Error/Error400.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Error400 = () => {
-	return <div className="error-message">Oops! Something went wrong :( Perhapes the url is incorrect</div>;
+const Error400 = ({ display }) => {
+	display = display.display === 'none' ? display : { display: 'block' };
+	return (
+		<div style={display} className="error-message">
+			Oops! Something went wrong :( Perhapes the url is incorrect or your image is too large...
+		</div>
+	);
 };
 
 export default Error400;
