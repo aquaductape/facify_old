5761c29dc2a4f6f8afaf3fab9505b111f4bf9cfc
Caleb Taylor <aquaductape@gmail.com>
2019-03-22 17:27:54 -0700

    added new loader for cropped images

---
+++ src/components/Loading/CroppedImages.js/CroppedImages.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CroppedImages = () => {
+	return (
+		<div className="container-loader">
+			<div className="lds-ring">
+				<div />
+				<div />
+				<div />
+				<div />
+			</div>
+		</div>
+	);
+};
+
+export default CroppedImages;
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Loading.css';
 import Base64Render from './Base64Render.js/Base64Render';
 import FaceDetection from './FaceDetection.js/FaceDetection';
+import CroppedImages from './CroppedImages.js/CroppedImages';
 
 const Loading = ({ loading }) => {
 	let render;
@@ -11,6 +12,9 @@ const Loading = ({ loading }) => {
 			break;
 		case 'yes:clarifai':
 			render = <FaceDetection />;
+			break;
+		case 'yes:client-images':
+			render = <CroppedImages />;
 			break;
 		default:
 			render = null;
